eaaf0e959de6fbbef0898a42748ffb22acdfd184
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-28 20:01:45 -0300

    refactor: improve ecs component composition readability

--- src/game/createPlayer.js
+++ src/game/createPlayer.js
@@ -1,4 +1,4 @@
-import { addComponent, createEntity } from "../ecs/world.js";
+import { addComponents, createEntity } from "../ecs/world.js";
 import {
   Collider,
   ComponentType,
@@ -9,38 +9,33 @@ import {
   Velocity,
 } from "../domain/components.js";
 
+const PLAYER_SIZE = 28;
+
 export function createPlayer(world) {
   const player = createEntity(world);
 
-  addComponent(world, player, ComponentType.Position, Position(96, 96));
-  addComponent(world, player, ComponentType.Velocity, Velocity());
-  addComponent(
-    world,
-    player,
-    ComponentType.Renderable,
-    Renderable({
-      shape: "rect",
-      width: 28,
-      height: 28,
-      color: "#f2c166",
-    }),
-  );
-  addComponent(
-    world,
-    player,
-    ComponentType.Collider,
-    Collider({
-      width: 28,
-      height: 28,
-    }),
-  );
-  addComponent(
-    world,
-    player,
-    ComponentType.MovementStats,
-    MovementStats({ speed: 180 }),
-  );
-  addComponent(world, player, ComponentType.PlayerControlled, PlayerControlled());
+  addComponents(world, player, [
+    [ComponentType.Position, Position(96, 96)],
+    [ComponentType.Velocity, Velocity()],
+    [
+      ComponentType.Renderable,
+      Renderable({
+        shape: "rect",
+        width: PLAYER_SIZE,
+        height: PLAYER_SIZE,
+        color: "#f2c166",
+      }),
+    ],
+    [
+      ComponentType.Collider,
+      Collider({
+        width: PLAYER_SIZE,
+        height: PLAYER_SIZE,
+      }),
+    ],
+    [ComponentType.MovementStats, MovementStats({ speed: 180 })],
+    [ComponentType.PlayerControlled, PlayerControlled()],
+  ]);
 
   return player;
 }
