b4a43ecffbb931b8bf517a115df9506421e2b007
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-28 20:38:12 -0300

    milestone 2: add basic health damage and enemy

--- src/app/main.js
+++ src/app/main.js
@@ -1,9 +1,12 @@
 import { createWorld } from "../ecs/world.js";
+import { createEnemy } from "../game/createEnemy.js";
 import { createPlayer } from "../game/createPlayer.js";
 import { createKeyboardInput } from "../input/keyboardInput.js";
 import { tilemap } from "../world/tilemap.js";
 import { playerControlSystem } from "../simulation/playerControlSystem.js";
 import { movementSystem } from "../simulation/movementSystem.js";
+import { testAttackSystem } from "../simulation/testAttackSystem.js";
+import { deathSystem } from "../simulation/deathSystem.js";
 import { createCanvasRenderer } from "../render/canvasRenderer.js";
 
 const canvas = document.querySelector("#game-canvas");
@@ -18,6 +21,7 @@ const keyboardInput = createKeyboardInput();
 const renderer = createCanvasRenderer(canvas, context);
 
 createPlayer(world);
+createEnemy(world);
 
 let lastFrameTime = performance.now();
 
@@ -26,9 +30,12 @@ function frame(currentTime) {
   lastFrameTime = currentTime;
 
   const movementIntent = keyboardInput.getMovementIntent();
+  const testAttackIntent = keyboardInput.consumeTestAttackIntent();
 
   playerControlSystem(world, movementIntent);
   movementSystem(world, tilemap, deltaSeconds);
+  testAttackSystem(world, testAttackIntent);
+  deathSystem(world);
   renderer.render(world, tilemap);
 
   requestAnimationFrame(frame);
