31fa9cb0ee287df1b39fe72aabe501c560890db3
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-29 17:23:45 -0300

    Run simple AI in simulation step

--- src/simulation/runSimulationStep.js
+++ src/simulation/runSimulationStep.js
@@ -1,7 +1,12 @@
 import { playerControlSystem } from "./playerControlSystem.js";
+import { aiSystem } from "./aiSystem.js";
 import { movementSystem } from "./movementSystem.js";
 import { actionEconomySystem } from "./actionEconomySystem.js";
-import { meleeCombatSystem } from "./meleeCombatSystem.js";
+import {
+  createAiMeleeAttackRequests,
+  createPlayerMeleeAttackRequests,
+  meleeCombatSystem,
+} from "./meleeCombatSystem.js";
 import { deathSystem } from "./deathSystem.js";
 
 export function runSimulationStep({
@@ -12,8 +17,12 @@ export function runSimulationStep({
   attackIntent,
 }) {
   playerControlSystem(world, movementIntent);
+  aiSystem(world);
   movementSystem(world, tilemap, deltaSeconds);
   actionEconomySystem(world, deltaSeconds);
-  meleeCombatSystem(world, attackIntent);
+  meleeCombatSystem(world, [
+    ...createPlayerMeleeAttackRequests(world, attackIntent),
+    ...createAiMeleeAttackRequests(world),
+  ]);
   deathSystem(world);
 }
