963c15a27ccc729b0394ed52a0e87dd50e2d44aa
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-30 16:59:43 -0300

    Run projectile systems in simulation step

--- src/simulation/runSimulationStep.js
+++ src/simulation/runSimulationStep.js
@@ -1,12 +1,15 @@
 import { playerControlSystem } from "./playerControlSystem.js";
 import { aiSystem } from "./aiSystem.js";
 import { movementSystem } from "./movementSystem.js";
+import { projectileMovementSystem } from "./projectileMovementSystem.js";
+import { projectileImpactSystem } from "./projectileImpactSystem.js";
 import { actionEconomySystem } from "./actionEconomySystem.js";
 import { meleeCombatSystem } from "./meleeCombatSystem.js";
 import {
   createAiMeleeAttackRequests,
   createPlayerMeleeAttackRequests,
 } from "./helpers/meleeAttackRequests.js";
+import { lifetimeSystem } from "./lifetimeSystem.js";
 import { deathSystem } from "./deathSystem.js";
 
 export function runSimulationStep({
@@ -19,10 +22,13 @@ export function runSimulationStep({
   playerControlSystem(world, movementIntent);
   aiSystem(world);
   movementSystem(world, tilemap, deltaSeconds);
+  projectileMovementSystem(world, tilemap, deltaSeconds);
+  projectileImpactSystem(world);
   actionEconomySystem(world, deltaSeconds);
   meleeCombatSystem(world, [
     ...createPlayerMeleeAttackRequests(commands),
     ...createAiMeleeAttackRequests(world),
   ]);
+  lifetimeSystem(world, deltaSeconds);
   deathSystem(world);
 }
