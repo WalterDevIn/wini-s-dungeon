e485465f53e5f805b159c610f2ce65411f5d7f0c
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-30 18:36:59 -0300

    Delegate player creation to creature factory

--- src/game/createPlayer.js
+++ src/game/createPlayer.js
@@ -1,65 +1,50 @@
-import { addComponents, createEntity } from "../ecs/world.js";
-import {
-  ActionEconomy,
-  AttackProfile,
-  Collider,
-  ComponentType,
-  Creature,
-  DamageReduction,
-  DefenseProfile,
-  Faction,
-  Health,
-  MovementStats,
-  PlayerControlled,
-  Position,
-  Renderable,
-  Velocity,
-} from "../domain/components.js";
+import { createCreature } from "../domain/factories/createCreature.js";
 
 const PLAYER_SIZE = 28;
 
 export function createPlayer(world) {
-  const player = createEntity(world);
-
-  addComponents(world, player, [
-    [ComponentType.Position, Position(96, 96)],
-    [ComponentType.Velocity, Velocity()],
-    [
-      ComponentType.Renderable,
-      Renderable({
-        shape: "glyph",
-        glyph: "@",
-        width: PLAYER_SIZE,
-        height: PLAYER_SIZE,
-        color: "#f5e6b8",
-        fontSize: 34,
-      }),
-    ],
-    [
-      ComponentType.Collider,
-      Collider({
-        width: PLAYER_SIZE,
-        height: PLAYER_SIZE,
-      }),
-    ],
-    [ComponentType.MovementStats, MovementStats({ speed: 180 })],
-    [ComponentType.PlayerControlled, PlayerControlled()],
-    [ComponentType.Health, Health({ current: 10, max: 10 })],
-    [ComponentType.Creature, Creature({ kind: "player" })],
-    [ComponentType.Faction, Faction({ id: "player" })],
-    [ComponentType.ActionEconomy, ActionEconomy()],
-    [
-      ComponentType.AttackProfile,
-      AttackProfile({
-        damage: 2,
-        range: 48,
-        windupSeconds: 0.1,
-        recoverySeconds: 0.35,
-      }),
-    ],
-    [ComponentType.DefenseProfile, DefenseProfile()],
-    [ComponentType.DamageReduction, DamageReduction({ flat: 0 })],
-  ]);
-
-  return player;
+  return createCreature(world, {
+    position: {
+      x: 96,
+      y: 96,
+    },
+    renderable: {
+      shape: "glyph",
+      glyph: "@",
+      width: PLAYER_SIZE,
+      height: PLAYER_SIZE,
+      color: "#f5e6b8",
+      fontSize: 34,
+    },
+    collider: {
+      width: PLAYER_SIZE,
+      height: PLAYER_SIZE,
+    },
+    movementStats: {
+      speed: 180,
+    },
+    controls: {
+      playerControlled: true,
+    },
+    health: {
+      current: 10,
+      max: 10,
+    },
+    creature: {
+      kind: "player",
+    },
+    faction: {
+      id: "player",
+    },
+    attackProfile: {
+      damage: 2,
+      range: 48,
+      windupSeconds: 0.1,
+      recoverySeconds: 0.35,
+    },
+    defenseProfile: {},
+    damageReduction: {
+      flat: 0,
+    },
+  });
 }
