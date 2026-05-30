020c6b7f29c49562e86b13654e41843ee51c0ca8
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-30 17:34:44 -0300

    Define firebolt as spell with cast timing

--- src/content/spells/firebolt.js
+++ src/content/spells/firebolt.js
@@ -1,12 +1,26 @@
 export const firebolt = Object.freeze({
   id: "firebolt",
-  projectile: Object.freeze({
-    speed: 260,
-    size: 10,
-    damage: 3,
-    lifetimeSeconds: 1.4,
-    color: "#ff7a33",
-    destroyOnWall: true,
-    destroyOnHit: true,
+  name: "Firebolt",
+  kind: "spell",
+  targeting: Object.freeze({
+    type: "point",
+  }),
+  cast: Object.freeze({
+    windupSeconds: 1.5,
+    recoverySeconds: 3,
+  }),
+  effect: Object.freeze({
+    type: "spawnProjectile",
+    projectile: Object.freeze({
+      speed: 260,
+      size: 10,
+      damage: 3,
+      lifetimeSeconds: 1.4,
+      color: "#ff7a33",
+      glyph: "x",
+      fontSize: 16,
+      destroyOnWall: true,
+      destroyOnHit: true,
+    }),
   }),
 });
