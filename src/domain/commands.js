986eb95706aabdb49119b699e8aff29cf71be12c
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-30 17:13:16 -0300

    Add cast command

--- src/domain/commands.js
+++ src/domain/commands.js
@@ -1,11 +1,27 @@
 export const CommandType = Object.freeze({
   Attack: "attack",
+  Cast: "cast",
 });
 
 export function AttackCommand({ actorId, source = "primaryAction" }) {
   return {
     type: CommandType.Attack,
     actorId,
+    source,
+  };
+}
+
+export function CastCommand({
+  actorId,
+  spellId,
+  targetPoint,
+  source = "secondaryAction",
+}) {
+  return {
+    type: CommandType.Cast,
+    actorId,
+    spellId,
+    targetPoint,
     source,
   };
 }
