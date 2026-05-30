9130efbe5bb801c2b7a2513868faa74fddcb8ca4
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-30 19:18:21 -0300

    Register basic creature roster

--- src/content/creatures/creatureRegistry.js
+++ src/content/creatures/creatureRegistry.js
@@ -1,9 +1,15 @@
+import { batDefinition } from "./bat.js";
 import { goblinSkirmisherDefinition } from "./goblinSkirmisher.js";
 import { humanAdventurerDefinition } from "./humanAdventurer.js";
+import { ratDefinition } from "./rat.js";
+import { stoneCrawlerDefinition } from "./stoneCrawler.js";
 
 const creatureDefinitions = Object.freeze({
+  [batDefinition.id]: batDefinition,
   [goblinSkirmisherDefinition.id]: goblinSkirmisherDefinition,
   [humanAdventurerDefinition.id]: humanAdventurerDefinition,
+  [ratDefinition.id]: ratDefinition,
+  [stoneCrawlerDefinition.id]: stoneCrawlerDefinition,
 });
 
 export function getCreatureDefinition(creatureId) {
