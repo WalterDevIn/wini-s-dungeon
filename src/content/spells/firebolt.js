c1c6e3b03fdd952d83aeebe624d5099d523f8b48
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-30 17:13:40 -0300

    Add firebolt spell definition

---
+++ src/content/spells/firebolt.js
@@ -0,0 +1,12 @@
+export const firebolt = Object.freeze({
+  id: "firebolt",
+  projectile: Object.freeze({
+    speed: 260,
+    size: 10,
+    damage: 3,
+    lifetimeSeconds: 1.4,
+    color: "#ff7a33",
+    destroyOnWall: true,
+    destroyOnHit: true,
+  }),
+});
