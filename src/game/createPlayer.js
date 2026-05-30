1cc4600a9db2de58c47d9fd8a0817842f80b7fb1
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-30 19:29:14 -0300

    Pass player spawn position to creature factory

--- src/game/createPlayer.js
+++ src/game/createPlayer.js
@@ -2,5 +2,10 @@ import { getCreatureDefinition } from "../content/creatures/creatureRegistry.js"
 import { createCreature } from "../domain/factories/createCreature.js";
 
 export function createPlayer(world) {
-  return createCreature(world, getCreatureDefinition("humanAdventurer"));
+  return createCreature(world, getCreatureDefinition("humanAdventurer"), {
+    position: {
+      x: 96,
+      y: 96,
+    },
+  });
 }
