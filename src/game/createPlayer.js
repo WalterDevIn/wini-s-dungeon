72e43b210d76d7f4851383262cde64641201695b
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-05-30 19:01:15 -0300

    Use human adventurer creature definition

--- src/game/createPlayer.js
+++ src/game/createPlayer.js
@@ -1,50 +1,6 @@
+import { getCreatureDefinition } from "../content/creatures/creatureRegistry.js";
 import { createCreature } from "../domain/factories/createCreature.js";
 
-const PLAYER_SIZE = 28;
-
 export function createPlayer(world) {
-  return createCreature(world, {
-    position: {
-      x: 96,
-      y: 96,
-    },
-    renderable: {
-      shape: "glyph",
-      glyph: "@",
-      width: PLAYER_SIZE,
-      height: PLAYER_SIZE,
-      color: "#f5e6b8",
-      fontSize: 34,
-    },
-    collider: {
-      width: PLAYER_SIZE,
-      height: PLAYER_SIZE,
-    },
-    movementStats: {
-      speed: 180,
-    },
-    controls: {
-      playerControlled: true,
-    },
-    health: {
-      current: 10,
-      max: 10,
-    },
-    creature: {
-      kind: "player",
-    },
-    faction: {
-      id: "player",
-    },
-    attackProfile: {
-      damage: 2,
-      range: 48,
-      windupSeconds: 0.1,
-      recoverySeconds: 0.35,
-    },
-    defenseProfile: {},
-    damageReduction: {
-      flat: 0,
-    },
-  });
+  return createCreature(world, getCreatureDefinition("humanAdventurer"));
 }
