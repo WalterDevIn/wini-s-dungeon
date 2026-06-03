b849ffa76427b4040b320216fde6b6e1d0b07303
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-03 19:37:34 -0300

    Inject generated encounter spawns

--- src/game/createDemoEncounter.js
+++ src/game/createDemoEncounter.js
@@ -1,39 +1,8 @@
 import { getCreatureDefinition } from "../content/creatures/creatureRegistry.js";
 import { createCreature } from "../domain/factories/createCreature.js";
 
-const demoEncounterSpawns = Object.freeze([
-  {
-    creatureId: "rat",
-    position: {
-      x: 96,
-      y: 96,
-    },
-  },
-  {
-    creatureId: "bat",
-    position: {
-      x: 576,
-      y: 480,
-    },
-  },
-  {
-    creatureId: "goblinSkirmisher",
-    position: {
-      x: 1632,
-      y: 480,
-    },
-  },
-  {
-    creatureId: "stoneCrawler",
-    position: {
-      x: 384,
-      y: 1104,
-    },
-  },
-]);
-
-export function createDemoEncounter(world) {
-  for (const spawn of demoEncounterSpawns) {
+export function createDemoEncounter(world, { spawns }) {
+  for (const spawn of spawns) {
     createCreature(world, getCreatureDefinition(spawn.creatureId), {
       position: spawn.position,
     });
