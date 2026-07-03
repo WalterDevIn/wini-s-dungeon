180b5208691dd69d8a8cd52d6343cbd1a098cc26
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-02 21:35:19 -0300

    Add mouse movement delta to pointer snapshot

--- src/input/mousePointerState.js
+++ src/input/mousePointerState.js
@@ -1,20 +1,31 @@
 export function createMousePointerState() {
   let pointerX = 0;
   let pointerY = 0;
+  let movementX = 0;
+  let movementY = 0;
   let hasPointerPosition = false;
 
   function updatePointerPosition(event) {
     pointerX = event.clientX;
     pointerY = event.clientY;
+    movementX += event.movementX || 0;
+    movementY += event.movementY || 0;
     hasPointerPosition = true;
   }
 
   function getSnapshot() {
-    return {
+    const snapshot = {
       x: pointerX,
       y: pointerY,
+      movementX,
+      movementY,
       hasPosition: hasPointerPosition,
     };
+
+    movementX = 0;
+    movementY = 0;
+
+    return snapshot;
   }
 
   return {
